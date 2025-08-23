f7eb37ddd162ffd1a9d6743c432630887b46dda5
CSueSue <christiaan@jemalu.nl>
2025-08-23 10:00:31 +0800

    added repro analysis to imageProcessing

--- imageGrabber.py
+++ imageGrabber.py
@@ -2,6 +2,8 @@
 
 
 from pypylon import pylon
+import cv2
+import time
 
 camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
 camera.Open()
@@ -11,10 +13,12 @@ new_width = camera.Width.Value - camera.Width.Inc
 if new_width >= camera.Width.Min:
     camera.Width.Value = new_width
 
-numberOfImagesToGrab = 100
-camera.StartGrabbingMax(numberOfImagesToGrab)
 
-while camera.IsGrabbing():
+count = 1000
+while count< 1100:#camera.IsGrabbing():
+
+    numberOfImagesToGrab = 1
+    camera.StartGrabbingMax(numberOfImagesToGrab)
     grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
 
     if grabResult.GrabSucceeded():
@@ -23,6 +27,10 @@ while camera.IsGrabbing():
         print("SizeY: ", grabResult.Height)
         img = grabResult.Array
         print("Gray value of first pixel: ", img[0, 0])
+        
+        cv2.imwrite("image_%i.tif" % count, img)
+        time.sleep(0.2)
+        count+=1
 
     grabResult.Release()
 camera.Close()
--- imageProcessing.py
+++ imageProcessing.py
@@ -6,7 +6,12 @@ from PIL import Image
 import cv2
 from scipy.optimize import least_squares
 from scipy.spatial import KDTree, distance
+from scipy.interpolate import RectBivariateSpline
 import time
+
+imdir = r'./testimages/'
+    
+
 pitch = 0.5e-3 
 N_spots = [19, 19]
 imsize = [2048, 2040]
@@ -115,6 +120,155 @@ def fitGaussian(gray):
 
     return fit_parms, sse_list
 
+
+
+
+
+def fitRectangle(gray, threshold = 75):
+
+    edge_clearance= 0.5 # fraction of square edge that is not fitted at corners.
+    Lcross = 10 
+    Sfilter = 9
+    subpixel = 0.1
+    outlier = 5
+    
+
+    thresh = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)[1]
+    
+    kernel = np.ones((5,5),np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(opening, 8, cv2.CV_32S)
+    
+    positions = centroids[1:,:]
+    
+    dist = distance.cdist(positions,positions,'euclidean').flatten()
+    pitch_median = np.median(dist[np.logical_and(dist>0.8*pitch/spix, dist<1.2*pitch/spix)])
+    
+    # find closest point for each point and calculate angle.
+    dist = distance.cdist(positions,positions,'euclidean')
+    dist[dist==0] = 1e4 # remove zeros.
+    Imin = np.argmin(dist, axis=0)
+    angles = np.arctan((positions[Imin,1]-positions[:,1])/(positions[Imin,0]-positions[:,0]))
+        
+    angles = np.mod(angles, np.pi/2)
+    phi = np.median(angles)
+    
+    width = np.median(stats[:,2])
+    height = np.median(stats[:,3])
+    Np = int(width*(1-edge_clearance))
+    
+    blurred_image = cv2.GaussianBlur(gray, (Sfilter, Sfilter), 0)
+    
+    ii = np.linspace(0.5*edge_clearance,1-0.5*edge_clearance,Np)
+    stats_rect = np.zeros((centroids.shape[0]-1,6))# x,y,w,h,phi, sigma
+    ipitch = int(pitch_median)
+    
+    # plt.figure(1, figsize = (21,21))
+    # plt.imshow(gray, cmap = "gray")
+    
+
+    
+    for i in range(centroids.shape[0]-1):
+        # interpolation of sub image.
+        Imin = max(int(centroids[i+1,1]-0.5*pitch_median),0)
+        Imax = min(ipitch+Imin,gray.shape[0])
+        Jmin = max(int(centroids[i+1,0]-0.5*pitch_median),0)
+        Jmax = min(ipitch+Jmin,gray.shape[1])
+        
+        xI = np.arange(Imax-Imin)
+        yI = np.arange(Jmax-Jmin)
+        
+        f_int = RectBivariateSpline(xI, yI ,blurred_image[Imin:Imax,Jmin:Jmax], kx = 3, ky = 3, s =0)
+        corners = corner_points_square([centroids[i+1,0], centroids[i+1,1],phi,width])
+        
+        
+        
+        edges= np.zeros((4,Np,2))
+        mu_edges = np.zeros((4,2))
+        for j in range(4):
+        
+    
+    
+            Pstart = corners[j,:].copy()
+            Pstart[0]-=Jmin
+            Pstart[1]-=Imin
+            
+            vec_dir = corners[j+1,:]-corners[j,:]
+            norm_dir = [vec_dir[1]*-1, vec_dir[0]]/np.linalg.norm(vec_dir)
+            
+            for k in range(Np):
+    
+                P0 = Pstart+ii[k]*vec_dir
+                
+                pointsx = P0[0] + np.arange(-Lcross,Lcross+subpixel,subpixel)*norm_dir[0]
+                pointsy = P0[1] + np.arange(-Lcross,Lcross+subpixel,subpixel)*norm_dir[1]
+                
+                dr = ((pointsx[1]-pointsx[0])**2 + ((pointsy[1]-pointsy[0])**2))**0.5
+                
+                I =f_int(pointsx,pointsy, grid = False)
+                
+                If = np.argmax(np.diff(I)/dr)
+                edges[j,k,:] = np.array([np.mean(pointsx[If:If+2]),np.mean(pointsy[If:If+2])])
+        
+                # plt.plot(edges[j,:,0]+Jmin, edges[j,:,1]+Imin,'r+')
+        
+            # rotate with angle -phi.
+            r_edge = (edges[j,:,0]*np.cos(-phi)+edges[j,:,1]*-np.sin(-phi))*((j%2)==0) +\
+                (edges[j,:,0]*np.sin(-phi)+edges[j,:,1]*np.cos(-phi))*((j%2)==1)
+            
+            # remove outliers and calculate mean
+            bool1 = abs(r_edge-r_edge.mean())<outlier
+            mu_edges[j,0]= r_edge[bool1].mean()
+            mu_edges[j,1]= r_edge[bool1].std()
+            
+            edges[j,np.logical_not(bool1),:] = np.nan
+        
+        x0 = (mu_edges[0,0]+mu_edges[2,0])/2+Jmin
+        y0 = (mu_edges[1,0]+mu_edges[3,0])/2+Imin           
+        w = mu_edges[0,0]-mu_edges[2,0]
+        h = mu_edges[1,0]-mu_edges[3,0]
+        sigma = np.sqrt(np.sum(mu_edges[:,1]**2)/mu_edges.shape[0])
+
+        stats_rect[i,0] = x0
+        stats_rect[i,1] = y0
+        stats_rect[i,2] = w
+        stats_rect[i,3] = h
+        stats_rect[i,4] = phi
+        stats_rect[i,5] = sigma
+        
+    # plt.plot(stats_rect[:,0], stats_rect[:,1], 'o')
+    
+    # plt.savefig("test.png")
+    # plt.show()
+    
+
+    return stats_rect
+
+def fitCentroids(gray, threshold = 75):
+   
+    thresh = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)[1]
+    
+    kernel = np.ones((5,5),np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(opening, 8, cv2.CV_32S)
+    
+    positions = centroids[1:,:]
+
+    
+    # find closest point for each point and calculate angle.
+    dist = distance.cdist(positions,positions,'euclidean')
+    dist[dist==0] = 1e4 # remove zeros.
+    Imin = np.argmin(dist, axis=0)
+    angles = np.arctan((positions[Imin,1]-positions[:,1])/(positions[Imin,0]-positions[:,0]))
+        
+    angles = np.mod(angles, np.pi/2)
+    phi = np.median(angles)
+    
+    stats_centroids = np.hstack((centroids[1:,:], stats[1:,2:4], phi*np.ones((centroids.shape[0]-1,1)) ))
+    return stats_centroids
+
 def calculateTilts(parameters):
     # indexing
     parameters = np.array(parameters)
@@ -122,17 +276,18 @@ def calculateTilts(parameters):
     
     # read nominal positions
     with open("nominalPositions.csv","r") as fp:
-        pos_nom = np.loadtxt(fp,  delimiter = ',')
-        
+        pos_nom = np.loadtxt(fp,  delimiter = ',')[:,:2]
+    
+    bool_nan = np.isnan(pos_nom[:,0])
     # find the closest point and index to nomimal positions. 
     tree = KDTree(parameters[:,:2])
-    dist, ii = tree.query(pos_nom)
+    dist, ii = tree.query(pos_nom[np.logical_not(bool_nan)])
     
-    
+    XY = np.zeros((pos_nom.shape))
     # calculate x,y vector for each point.
-    XY = parameters[ii,:2]-pos_nom
+    XY[np.logical_not(bool_nan)] = parameters[ii,:2]-pos_nom[np.logical_not(bool_nan)]
     # put distances > 0.25*pitch to nan.
-    XY[dist>0.25*pitch/spix,:] = np.nan
+    XY[np.logical_not(bool_nan)][dist>0.25*pitch/spix,:] = np.nan
     
     
     # calculate tilts. Signs: Ry ~ x , Rx ~ -y. 
@@ -141,14 +296,11 @@ def calculateTilts(parameters):
     tiltMeasurement[:,:,1] = -XY[:,1].reshape(N_spots)*spix/(2*FL)
     return tiltMeasurement
 
-def calibrateNominalPositions(positions):
+def calibrateNominalPositions(positions, Rz):
     # calculate pitch
     dist = distance.cdist(positions,positions,'euclidean').flatten()
     pitch_median = np.median(dist[np.logical_and(dist>0.8*pitch/spix, dist<1.2*pitch/spix)])
-    # calculate Rz.
-    Rz = np.cov(positions[:,0],positions[:,1])/np.cov(positions[:,0])
-    Rz = Rz[0,1]
-    
+
     # rotate to zero.
     Xr = positions[:,0]*np.cos(-Rz) -positions[:,1]*np.sin(-Rz)
     Yr = positions[:,0]*np.sin(-Rz) +positions[:,1]*np.cos(-Rz)
@@ -159,34 +311,113 @@ def calibrateNominalPositions(positions):
     Iy = np.round(Yr/pitch_median).astype(int)
     Iy -=Iy.min()
     
-    idx_sorted = np.zeros(Ix.shape, int)
-    # sort in y-direction, then sort in x -direction.
-    Iy_uniq = np.unique(Iy)
-    j = 0
-    for i,iy in enumerate(Iy_uniq):
-        ii = np.where(Iy==iy)[0]
-        idx_sorted[j:j+ii.shape[0]] = ii[np.argsort(Ix[ii])]
-        j+= ii.shape[0]
+    sorted_data = np.zeros((N_spots[0]*N_spots[1],2))
+    for i in range(N_spots[0]):
+        for j in range(N_spots[1]):
+            idx =np.where(np.logical_and(Ix==j, Iy==i))[0]
+            if len(idx)>0:
+
+                sorted_data[i*N_spots[0]+j,:] = positions[idx[0],:]
+            else:
+                sorted_data[i*N_spots[0]+j,:] = np.nan
+
     
+    with open("nominalPositions.csv","w") as fp:   
+       np.savetxt(fp, sorted_data, delimiter = ',')
+
+def corner_points_square(parms):
+    x0  = parms[0]
+    y0  = parms[1]
+    Rz  = parms[2]
+    W = parms[3]
+    corners = np.array([ [0.5*W,-0.5*W], [0.5*W,0.5*W],[-0.5*W,0.5*W],[-0.5*W,-0.5*W],[0.5*W,-0.5*W]])
+    # rotate + translate
+    xcorners = np.cos(Rz)*corners[:,0] -np.sin(Rz)*corners[:,1]+x0
+    ycorners = np.sin(Rz)*corners[:,0] +np.cos(Rz)*corners[:,1]+y0
     
-    with open("nominalPositions.csv","w") as fp:
-       np.savetxt(fp, positions[idx_sorted,:], delimiter = ',')
+    return np.vstack((xcorners,ycorners)).T
+
+
+def fit_images_test_dir():
+    
+    imdir = r'./testimages/'
+    
+    for i in range(100):
+        f_im = "image_%i.tif" % (1000+i)
+        im_pil = Image.open(imdir+f_im)
+        #Convert the image to a NumPy array
+        gray = np.array(im_pil)
+    
+        #stats_rectangle = fitRectangle(gray)
+        stats_rectangle = fitCentroids(gray)
+        
+        with open(imdir + f_im.split('.')[0]+ ".csv" , "w") as fp:
+            np.savetxt(fp,stats_rectangle, delimiter = ',')
+
+def calibrate_with_first_test_im():
+
+
+    f_im = "image_1000.csv"
+    with open(imdir+f_im,'r') as fp:
+        stats_rectangle = np.loadtxt(fp, delimiter = ',')
+    
+    calibrateNominalPositions(stats_rectangle[:,:2], stats_rectangle[0,4])
+    
+def calculate_repro():
+
+    
+    Rx = np.zeros((19*19, 99))
+    Ry = np.zeros((19*19, 99))
+    for i in range(1,100):
+        f_im = "image_%i.csv" % (1000+i)
+        with open(imdir+f_im,'r') as fp:
+            stats_rectangle = np.loadtxt(fp, delimiter = ',')
+            
+        tilts= calculateTilts(stats_rectangle[:,:2])
+        
+        Rx[:,i-1]= tilts[:,:,0].flatten()
+        Ry[:,i-1]= tilts[:,:,1].flatten()
+        
+    
+    # subtract average 
+    Rx_mean = np.mean(Rx, axis=0)
+    Ry_mean = np.mean(Ry, axis=0)
+    Rx = Rx-np.tile(Rx_mean,(Rx.shape[0],1))
+    Ry = Ry-np.tile(Ry_mean,(Ry.shape[0],1))
+    
+    # calculate std.
+    sRx = np.std(Rx, axis = 1)
+    sRy = np.std(Ry, axis = 1)    
+    
+    #fig = plt.figure(1)
+    fig, ax = plt.subplots()
+    im= ax.imshow(sRx.reshape(19,19))
+    fig.colorbar(im, ax=ax, label = "Rx repro- std[rad]")
+        
+    
+    fig, ax = plt.subplots()
+    im= ax.imshow(sRy.reshape(19,19))
+    fig.colorbar(im,ax=ax, label = "Ry repro- std[rad]")
+    plt.show()
+    
+    # calculate average repro number on center of image.
+    print("repro Rx[rad] 1sigma", sRx.reshape(19,19)[2:-2,2:-2].flatten().mean())
+    print("repro Ry[rad] 1sigma", sRy.reshape(19,19)[2:-2,2:-2].flatten().mean())
+    
 
 if __name__ == "__main__":
+    #fit_images_test_dir()
+    calibrate_with_first_test_im()
     
-    im_pil = Image.open("testimage.png")
-    #Convert the image to a NumPy array
-    gray = np.array(im_pil)
-
-
-    parameters, sse = fitGaussian(gray)
-    
-    parameters = np.array(parameters)
+    calculate_repro()
     
     
-    tiltMeasurement = calculateTilts(parameters)
     
-
-        
-        
-    #calibrateNominalPositions(parameters[:,:2])
+    #plt.show()
+    # for i in range(30):
+    #     plt.figure(1+i)
+    #     plt.plot(Rx[i*10:i*10+10,:].T)
+    # plt.show()
+    
+    # plt.plot(Rx_mean)
+    # plt.plot(np.median(Rx,axis=0))
